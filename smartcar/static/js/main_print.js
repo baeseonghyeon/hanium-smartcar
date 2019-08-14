d4b98757408141cf8120de23aaaac383262dad7a
seonghyeon <tjdgusdkf@gmail.com>
2019-08-15 04:24:19 +0900

    css

--- smartcar/static/js/main_print.js
+++ smartcar/static/js/main_print.js
@@ -19,11 +19,15 @@ function create_car(Car){
         success : function (data) {
             car = new Array();
             for(var i=1; i<=Car.length; i++){
-                var cardiv = document.createElement("div");
-                cardiv.setAttribute("id", i-1);
-                cardiv.setAttribute("class", 'cardiv');
-                cardiv.setAttribute('OnClick', 'car_detail(this.id)');
-                cardiv.innerHTML = data[i-1].car_name;
+				var car_state_div = document.createElement("div");
+                var car_name_div = document.createElement("div");
+				car_state_div.setAttribute("id", i-1);
+				car_name_div.setAttribute("id", i-1);
+				car_state_div.setAttribute("class", 'car_state_div');
+				car_name_div.setAttribute("class", 'car_name_div');
+				car_state_div.setAttribute('OnClick', 'car_detail(this.id)');
+                car_name_div.setAttribute('OnClick', 'car_detail(this.id)');
+                car_name_div.innerHTML = data[i-1].car_name;
                 pi_pi[i-1] = data[i-1].pi_id
                 $.ajax({
                     url : "http://127.0.0.1:8000/api/PiInfo/?format=json",
@@ -31,12 +35,13 @@ function create_car(Car){
                     success : function (pi_data) {
                         for(var j=0; j<=Car.length; j++){
                             if(pi_pi[j]==pi_data[j].pi_id){
-                                cardiv.innerHTML=pi_data[j].car_type
+                                car_name_div.innerHTML=pi_data[j].car_type
                             }
                         }
                     }
-                });
-                $("#main-side2").append(cardiv);
+				});
+				$("#main-side2").append(car_state_div);
+                $("#main-side2").append(car_name_div);
             }
         }
     });
