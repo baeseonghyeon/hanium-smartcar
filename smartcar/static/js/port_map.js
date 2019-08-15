062a8c31d482f1a206863cf5884312b336b6eefe
seonghyeon <tjdgusdkf@gmail.com>
2019-08-15 20:24:57 +0900

    bsh

--- smartcar/static/js/port_map.js
+++ smartcar/static/js/port_map.js
@@ -31,7 +31,7 @@ function map(){
                                  var map_span = document.createElement("span");
                                  map_span.setAttribute("id", "route1");
                                  map_span.setAttribute("class", i+'a'+j);
-                                 map_span.innerHTML = 'Con';
+                                 map_span.innerHTML = 'Con<br>'+i+'-'+j;
                                  $("#map").append(map_span);
                             }
                             else if(map_array2[i][j]==2){
